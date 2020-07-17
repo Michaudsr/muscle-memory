d18b691b1235e0007f24a3179baabd1c18f818d1
Steven Michaud <stevenmichaud@Stevens-MacBook-Air.local>
2020-07-17 17:10:02 -0400

    [variable] add 5 variables

---
+++ app.js
@@ -0,0 +1,26 @@
+// 5 variables of different types
+// string
+//number
+// array
+// boolean
+// object (4 different key/value pairs) [string], [number], [array], [boolean]
+
+
+let string = "This is a string";
+console.log(sentence);
+
+let number = 5;
+console.log(number);
+
+let array = [5,6,7,8,9];
+console.log(array);
+
+let number = 2 > 4;
+console.log(number);
+
+let object = {
+    car = 'chevy',
+    make = 'silverado',
+    year = '2016',
+}
+console.log(object);
