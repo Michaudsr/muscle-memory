fe990179fc3de7d034cac5aa6b450c3c34efcbe4
Steven Michaud <stevenmichaud@Stevens-MacBook-Air.local>
2020-07-17 18:20:58 -0400

    [steven] muscle memory

--- app.js
+++ app.js
@@ -7,7 +7,7 @@
 
 
 let string = "This is a string";
-console.log(sentence);
+console.log(string);
 
 let number = 5;
 console.log(number);
@@ -15,12 +15,79 @@ console.log(number);
 let array = [5,6,7,8,9];
 console.log(array);
 
-let number = 2 > 4;
+let something = true;
 console.log(number);
 
 let object = {
-    car = 'chevy',
-    make = 'silverado',
-    year = '2016',
+    car: 'chevrolet',
+    make: 'silverado',
+    year: '2016',
 }
 console.log(object);
+
+function printStuff(){
+    let name = 'Steven';
+}
+
+// make 10 different functions
+// one of your function need to pass in an array
+// one of your functions need to pass in a callback
+// ....string
+// ....object
+// ....boolean
+// ....freestyle
+
+// i need help with this I'm going to get some help after class
+
+function displayCoolBikes(array) {
+    let result = [];
+
+    for (let i =0; i < array.length; i++){
+
+    let bike = array[i];
+
+    if (bike.length > 7){
+        result.push(bike);
+
+    }
+
+    }
+}
+
+// return result;
+
+// }
+
+// // // console.log(displayCoolBikes(motorcycles));
+
+// // function removeSentence(string){
+// //     let result = ;
+// // }
+
+// // removeFromArray(displayCoolBikes, motorcycles);
+
+const sectionOne = document.getElementById('section-one')
+console.log(sectionOne);
+
+const sectionTwo = document.getElementById('section-two')
+console.log(sectionTwo);
+
+const sectionThree = document.getElementById('section-three')
+console.log(sectionThree);
+
+const sectionFour = document.getElementById('section-four')
+console.log(sectionFour);
+
+const sectionFive = document.getElementById('section-five')
+console.log(sectionFive);
+
+sectionOne.textContent = 'Lakers';
+sectionTwo.textContent = 'Warriors';
+sectionThree.textContent = 'Celtics';
+sectionFour.textContent = 'Nets';
+sectionFive.textContent = 'Blazers';
+
+
+
+
+
